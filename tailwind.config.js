e607e07e31e4d033cd27b3414e2f6d5f4be213e6
akmalays <akmalays@gmail.com>
2022-04-16 14:34:48 +0700

    fix config, install tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
   theme: {
     extend: {},
   },
